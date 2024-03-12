b205ec70dc10e6e979d9257d0dcc6c13f45a776d
Steven Lam <stevenlam3535@gmail.com>
2024-03-11 20:11:35 -0400

    Add MapObserver.cpp and MapObserver.h

---
+++ Models/MapObserver.cpp
@@ -0,0 +1,16 @@
+#include "MapObserver.h"
+#include <iostream>
+#include "Map.h"
+
+// Constructor definition
+MapObserver::MapObserver() 
+{
+    // Constructor body (if any initialization is needed)
+}
+
+// Implement the update method
+void MapObserver::update() 
+{
+    
+}
+
---
+++ Models/MapObserver.h
@@ -0,0 +1,18 @@
+#ifndef MAPOBSERVER_H
+#define MAPOBSERVER_H
+
+class Map; 
+
+//Implementation of the MapObserver Object that will observe the Map 
+class MapObserver
+{
+    public:
+    MapObserver(); //constructor 
+    void update(); // Method to update the observer with the current state of the map
+
+
+    private: 
+
+}; 
+
+#endif
