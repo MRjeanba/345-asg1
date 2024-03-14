8604c0bb4c26a1d2e18a51e78c1129a3150eee33
MRjeanba <jeanba19@outlook.fr>
2024-03-13 20:41:42 -0400

    fix driver issue

--- Controllers/MapEditorController.cpp
+++ Controllers/MapEditorController.cpp
@@ -4,7 +4,13 @@
 
 #include "MapEditorController.h"
 #include "../Utilities.h"
-
+inline void loadMap(Map &mapToFill, const std::string &filename)
+{
+    const std::string completeFileName = filename + ".xml";
+    std::ifstream is(std::filesystem::current_path() / "../MapsXML/" / completeFileName);
+    cereal::XMLInputArchive archive(is);
+    archive(mapToFill);
+}
 
 MapEditorController::MapEditorController() {
 
